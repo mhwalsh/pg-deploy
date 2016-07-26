57ee56724cf85b0c5d0d5a35422671f6c6bb8a84
Millie Walsh <millie@primeacademy.io>
2016-07-26 13:05:19 -0500

    set ssl differently

--- modules/connection.js
+++ modules/connection.js
@@ -1,11 +1,14 @@
-// require('dotenv').config();
+var pg = require('pg');
 var connectionString = '';
 
 if(process.env.DATABASE_URL !== undefined) {
     console.log('env connection string');
-    connectionString = process.env.DATABASE_URL + 'ssl';
+    connectionString = process.env.DATABASE_URL;
+    pg.defaults.ssl = true;
 } else {
     connectionString = 'postgres://localhost:5432/foodDb';
 }
+
+console.log("connectionString set to: ", connectionString);
 
 module.exports = connectionString;
